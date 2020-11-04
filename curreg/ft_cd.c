db844f5836d264220e393cef1ca92cbc9cf65de8
vip8084 <vip8084@mail.ru>
2020-11-04 18:54:26 +0300

    fixed some leaks

--- curreg/ft_cd.c
+++ curreg/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: curreg <curreg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 11:07:05 by hmiso             #+#    #+#             */
-/*   Updated: 2020/11/04 17:17:03 by curreg           ###   ########.fr       */
+/*   Updated: 2020/11/04 18:53:24 by curreg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,21 @@ void		ft_cd(char **comand_line, t_vars *vars)
 	path = NULL;
 	if(comand_line[0 + 1] != NULL)
 	{
-		path = comand_line[1];    ////////
+		path = comand_line[1];  
 		chdir(path);
-		//free_two_dimensional_array(comand_line);
-		free(path);       /////////
-		path = NULL;      //////////
+		free_two_dimensional_array(comand_line);
+		path = NULL;      
 	}
 	else
 	{
 		path = init_patch(vars, "HOME");
 		chdir(path);
+		free_two_dimensional_array(comand_line);
 		free(path);
 		path = NULL;
 	}
 }
 
-//избавиться от утечек
 //настроить возврат сообщения о не правильной директории
 //корректно установить код ошибки
 //проверить возрат баша на папку к которой нет прав в том числе код ошибки

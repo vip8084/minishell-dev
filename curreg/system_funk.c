cedb2e5b5703a0fb08dd0b5b2dd1b3052ff0bd3f
vip8084 <vip8084@mail.ru>
2020-11-07 19:33:26 +0300

    all is working

--- curreg/system_funk.c
+++ curreg/system_funk.c
@@ -6,7 +6,7 @@
 /*   By: curreg <curreg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 16:42:45 by hmiso             #+#    #+#             */
-/*   Updated: 2020/11/07 19:00:38 by curreg           ###   ########.fr       */
+/*   Updated: 2020/11/07 19:16:54 by curreg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,20 +23,18 @@ void			system_funk(char *path, char **argv, t_vars *vars)//вызов систе
 	if (pid == 0)
 	{
 		if ((status = execve(path, argv, vars->envp_copy)) == -1)
+		{
+			set_g_error(errno);
+			command_error(argv[0], vars);
 			exit(WEXITSTATUS(status));
+		}
 	}
 	else if (pid < 0)
 		ft_putendl_fd("error", 2);
 	else
-	{
-		if ((status = execve(path, argv, vars->envp_copy)) == -1)
-		{
-			set_g_error(errno);
-			command_error(argv[0], vars);
-		}
-			
-		waitpid(pid, &status, WUNTRACED);               //////
-		vars->g_exit_code = WEXITSTATUS(status);        /////
+	{	
+		waitpid(pid, &status, WUNTRACED);             
+		vars->g_exit_code = WEXITSTATUS(status);
 	}
 }
 //нужно разобраться с установкой ошибок

93de7d2da2d8a76936d1303f2df92ae0d676aadc
vip8084 <vip8084@mail.ru>
2020-11-12 20:02:10 +0300

    working but not realized #?

--- curreg/errors.c
+++ curreg/errors.c
@@ -6,36 +6,59 @@
 /*   By: curreg <curreg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/07 17:51:37 by curreg            #+#    #+#             */
-/*   Updated: 2020/11/09 18:48:38 by curreg           ###   ########.fr       */
+/*   Updated: 2020/11/12 19:45:36 by curreg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishel.h"
 
+// int get_exit_err()
+// {
+//     int error;
+    
+//     error = 0;
+//     vars->err_flag;
+//     vars->cd_flag;
+//        g_error;
+
+//     return (error);
+// }
+
+
 void command_error(char *cmd, t_vars *vars)
 {
-    ft_putstr_fd("minishell>", 1);
-    if (ft_strcmp("$?", cmd) == 0)
+    
+
+    if(ft_strcmp("clear", cmd) == 0)
     {
-        if (vars->cd_flag == 1 && vars->err_flag == 0)
+        vars->err_flag = 0;
+        g_error = 0;
+    }
+    else
+    {
+        ft_putstr_fd("minishell>", 1);
+        if (ft_strcmp("$?", cmd) == 0)
         {
-            ft_putstr_fd("1", 1);
-            vars->err_flag = 1;
-        }
-        else if (vars->err_flag == 0)
-        {
-            ft_putstr_fd("0", 1);
-            vars->err_flag = 1;
+            if (vars->cd_flag == 1 && vars->err_flag == 0)
+            {
+                ft_putstr_fd("1", 1);
+                vars->err_flag = 1;
+            }
+            else if (vars->err_flag == 0)
+            {
+                ft_putstr_fd("0", 1);
+                vars->err_flag = 1;
+            }
+            else
+                ft_putstr_fd(ft_itoa(g_error), 1);
         }
         else
-            ft_putstr_fd(ft_itoa(g_error), 1);
+        {
+            vars->err_flag = 1;
+            ft_putstr_fd(cmd, 1);
+        }
+	    ft_putstr_fd(": command not found\n", 1);
     }
-	else
-    {
-        vars->err_flag = 1;
-        ft_putstr_fd(cmd, 1);
-    }
-	ft_putstr_fd(": command not found\n", 1);
 }
 
 void cd_error(char *cmd, char* arg, int err)
--- curreg/system_funk.c
+++ curreg/system_funk.c
@@ -6,7 +6,7 @@
 /*   By: curreg <curreg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 16:42:45 by hmiso             #+#    #+#             */
-/*   Updated: 2020/11/09 20:59:15 by curreg           ###   ########.fr       */
+/*   Updated: 2020/11/12 19:56:17 by curreg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,43 +23,16 @@ void			system_funk(char *path, char **argv, t_vars *vars)//вызов систе
 	if (pid == 0)
 	{
 		if ((status = execve(path, argv, vars->envp_copy)) == -1)
-			exit(127);
-
-		// if ((status = execve(path, argv, vars->envp_copy)) == -1)
-		// {
-		// 	set_g_error(errno);
-		// 	command_error(argv[0], vars);
-		//	exit(WEXITSTATUS(status));
-		// }
+			exit(errno);
 	}
 	else if (pid < 0)
 		ft_putendl_fd("error", 2);
 	else
 	{
-		// if ((status = execve(path, argv, vars->envp_copy)) == -1)
-		// {
-		// 	set_g_error(errno);
-		// 	command_error(argv[0], vars);
-		// }
-			
-		waitpid(pid, &status, WUNTRACED);               //////
+		waitpid(pid, &status, WUNTRACED);
 		if (WIFEXITED(status))
-			g_error = WEXITSTATUS(status);
-
-		printf("\n");
-		printf("%d\n", g_error);
-		printf("\n");
+			set_g_error(WEXITSTATUS(status));
+		command_error(argv[0], vars);
 		vars->g_exit_code = WEXITSTATUS(status);        /////
-
-
-		// if(!(pid=fork()))
-		// {
-		// 	exit(1);
-		// }
-		// waitpid(pid,&status,0);
-		// if (WIFEXITED(status)) {
-		// 	printf("%d", WEXITSTATUS(status));
-		// }
 	}
 }
-//нужно разобраться с установкой ошибок

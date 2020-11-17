4186daddda934fc22522a73b7eff5b088f429d0f
SWdvcicgIE1hY2tld2ljaCA= TWFja2V3aWNoIA== <hmiso@il-c4.msk.21-school.ru>
2020-11-17 17:30:52 +0300

    need to sort out this

--- curreg/ft_echo.c
+++ curreg/ft_echo.c
@@ -6,14 +6,67 @@
 /*   By: hmiso <hmiso@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 11:46:29 by hmiso             #+#    #+#             */
-/*   Updated: 2020/11/17 17:26:42 by hmiso            ###   ########.fr       */
+/*   Updated: 2020/11/17 17:29:42 by hmiso            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../minishel.h"
 
-void			ft_echo(char **line, t_vars *vars)
+// void			ft_echo(char **line, t_vars *vars)
+// {
+// 	int i;
+// 	char *path;
+	
+// 	i = 1;
+// 	path = NULL;
+// 	while(line[i] != NULL)
+// 	{
+// 		if (line[i][0] == '$')
+// 		{
+// 			if(line[i][1] == '?')
+// 			{
+// 				show_g_error();
+// 				vars->err_flag = 0;
+// 				vars->cd_flag = 0;
+// 			}
+// 			path = init_patch(vars, &line[i][1]);
+// 			ft_putstr_fd(path, 1);
+// 			free(path);
+// 			path = NULL;
+// 			i++;
+// 			if(line[i] != NULL)
+// 				write(1, " ", 1);
+// 		}
+// 		else
+// 		{
+// 		// if (line[i][0] == '$')
+// 		// {
+// 		// 	path = init_patch(vars, &line[i][1]);
+// 		// 	ft_putstr_fd(path, 1);
+// 		// 	free(path);
+// 		// 	path = NULL;
+// 		// 	i++;
+// 		// 	if(line[i] != NULL)
+// 		// 		write(1, " ", 1);
+// 		// }
+// 		// else
+// 		// {
+// 			ft_putstr_fd(line[i], 1);
+// 			i++;
+// 			if(line[i] != NULL)
+// 				write(1, " ", 1);			
+// 		// }
+// 	}
+// 	free_two_dimensional_array(line);
+// 	write(1, "\n", 1);
+// }
+
+// не правильно работает с вызовом в несколько аргументов
+// парсинг экранирования кавычек и всего еже с ним
+
+
+void			ft_echo(char **line, t_vars *vars)//команда echo 
 {
 	int i;
 	char *path;
@@ -22,24 +75,6 @@ void			ft_echo(char **line, t_vars *vars)
 	path = NULL;
 	while(line[i] != NULL)
 	{
-		if (line[i][0] == '$')
-		{
-			if(line[i][1] == '?')
-			{
-				show_g_error();
-				vars->err_flag = 0;
-				vars->cd_flag = 0;
-			}
-			path = init_patch(vars, &line[i][1]);
-			ft_putstr_fd(path, 1);
-			free(path);
-			path = NULL;
-			i++;
-			if(line[i] != NULL)
-				write(1, " ", 1);
-		}
-		else
-		{
 		// if (line[i][0] == '$')
 		// {
 		// 	path = init_patch(vars, &line[i][1]);

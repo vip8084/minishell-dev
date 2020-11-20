df93c82447edbab4bbcfb07383cab26d9e3e86ab
vip8084 <vip8084@mail.ru>
2020-11-20 14:24:10 +0300

    some shit

--- curreg/ft_echo.c
+++ curreg/ft_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hmiso <hmiso@student.42.fr>                +#+  +:+       +#+        */
+/*   By: curreg <curreg@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 11:46:29 by hmiso             #+#    #+#             */
-/*   Updated: 2020/11/17 17:29:42 by hmiso            ###   ########.fr       */
+/*   Updated: 2020/11/17 17:40:37 by curreg           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@
 // 			i++;
 // 			if(line[i] != NULL)
 // 				write(1, " ", 1);			
-// 		// }
+// 		}
 // 	}
 // 	free_two_dimensional_array(line);
 // 	write(1, "\n", 1);
@@ -65,8 +65,9 @@
 // не правильно работает с вызовом в несколько аргументов
 // парсинг экранирования кавычек и всего еже с ним
 
+//////////////////////
 
-void			ft_echo(char **line, t_vars *vars)//команда echo 
+void			ft_echo(char **line, t_vars *vars)
 {
 	int i;
 	char *path;
@@ -75,27 +76,69 @@ void			ft_echo(char **line, t_vars *vars)//команда echo
 	path = NULL;
 	while(line[i] != NULL)
 	{
-		// if (line[i][0] == '$')
-		// {
-		// 	path = init_patch(vars, &line[i][1]);
-		// 	ft_putstr_fd(path, 1);
-		// 	free(path);
-		// 	path = NULL;
-		// 	i++;
-		// 	if(line[i] != NULL)
-		// 		write(1, " ", 1);
-		// }
-		// else
-		// {
+		if (line[i][0] == '$')
+		{
+			if(line[i][1] == '?')
+			{
+				show_g_error();
+				vars->err_flag = 0;
+				vars->cd_flag = 0;
+			}
+			path = init_patch(vars, &line[i][1]);
+			ft_putstr_fd(path, 1);
+			free(path);
+			path = NULL;
+			i++;
+			if(line[i] != NULL)
+				write(1, " ", 1);
+		}
+		else
+		{
 			ft_putstr_fd(line[i], 1);
 			i++;
 			if(line[i] != NULL)
 				write(1, " ", 1);			
-		// }
+		}
 	}
 	free_two_dimensional_array(line);
 	write(1, "\n", 1);
 }
+
+
+
+/////////////////////
+
+
+// void			ft_echo(char **line, t_vars *vars)//команда echo 
+// {
+// 	int i;
+// 	char *path;
+	
+// 	i = 1;
+// 	path = NULL;
+// 	while(line[i] != NULL)
+// 	{
+// 		// if (line[i][0] == '$')
+// 		// {
+// 		// 	path = init_patch(vars, &line[i][1]);
+// 		// 	ft_putstr_fd(path, 1);
+// 		// 	free(path);
+// 		// 	path = NULL;
+// 		// 	i++;
+// 		// 	if(line[i] != NULL)
+// 		// 		write(1, " ", 1);
+// 		// }
+// 		// else
+// 		// {
+// 			ft_putstr_fd(line[i], 1);
+// 			i++;
+// 			if(line[i] != NULL)
+// 				write(1, " ", 1);			
+// 		// }
+// 	}
+// 	free_two_dimensional_array(line);
+// 	write(1, "\n", 1);
+// }
 
 // не правильно работает с вызовом в несколько аргументов
 // парсинг экранирования кавычек и всего еже с ним
